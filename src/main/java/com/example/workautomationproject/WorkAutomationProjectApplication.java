fcf9c2ca52dcfa6bfb4cd96db6a4721278cd4dd2
Nickolodeon98 <sjeon0730@gmail.com>
2024-04-30 23:20:33 +0900

    feat: start a new project

---
+++ src/main/java/com/example/workautomationproject/WorkAutomationProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.workautomationproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkAutomationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkAutomationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/workautomationproject/WorkAutomationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.workautomationproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkAutomationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
